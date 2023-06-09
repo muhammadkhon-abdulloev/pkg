a186efbe8c728f2a147fa5b7450989013a0c481d
Muhammadkhon <muhammadkhon@MacBook-Pro-Muhammadkhon.local>
2023-06-09 16:35:02 +0300

    env tag for struct

--- logger/config.go
+++ logger/config.go
@@ -1,10 +1,10 @@
 package logger
 
 type Config struct {
-	AddCallerSkip int    `json:"addCallerSkip"`
-	Decoder       string `json:"decoder"`
-	FilePath      string `json:"filePath"`
-	Level         string `json:"level"`
-	Production    bool   `json:"development"`
-	ToFile        bool   `json:"toFile"`
+	AddCallerSkip int    `json:"addCallerSkip" env:"LOGGER_ADD_CALLER_SKIP"`
+	Decoder       string `json:"decoder" env:"LOGGER_DECODER"`
+	FilePath      string `json:"filePath" env:"LOGGER_FILE_PATH"`
+	Level         string `json:"level" env:"LOGGER_LEVEL"`
+	Production    bool   `json:"development" env:"LOGGER_PRODUCTION"`
+	ToFile        bool   `json:"toFile" env:"LOGGER_TO_FILE"`
 }
